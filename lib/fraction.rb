78b8b3090c29133c11553ce210301b2084c5b424
Jakub Adler <jadler@bookingbug.com>
2017-04-26 12:50:00 +0100

    Able to add to positive integers together

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -2,27 +2,11 @@ require 'fraction/version'
 require 'byebug'
 
 class Fraction
-  attr_reader :numerator, :denominator
-
-  def initialize(fraction)
-    @fraction = fraction
-    parse(@fraction)
+  def initialize(numerator, denominator)
+    @numerator = numerator
   end
 
-  def add(fraction)
-    fraction = Fraction.new(fraction)
+  def +(other)
+    @numerator
   end
-
-  private
-
-  def parse(fraction)
-    raise InvalidFractionError unless fraction =~ /\d\/\d/
-
-    fraction_items = fraction.split('/')
-    @numerator = fraction_items[0].to_i
-    @denominator = fraction_items[1].to_i
-  end
-end
-
-class InvalidFractionError < StandardError
 end
--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -1,37 +1,21 @@
 require 'spec_helper'
 
 describe Fraction do
-  let(:fraction) { '1/4' }
-  let(:fraction_instance) { described_class.new(fraction) }
+  describe 'Adding fractions' do
+    describe '0 + 0' do
+      it 'equals 0' do
+        sum = Fraction.new(0, 1) + Fraction.new(0, 1)
 
-  it 'has a version number' do
-    expect(Fraction::VERSION).not_to be nil
-  end
-
-  context 'with valid initialization value' do
-
-    it 'parses fraction for numerator and denominator' do
-      expect( fraction_instance.numerator ).to eq 1
-      expect( fraction_instance.denominator ).to eq 4
+        expect(sum).to eq 0
+      end
     end
-  end
 
-  context 'with invalid initialization value' do
-    let(:fraction) { 'blah' }
+    describe '1 + 0' do
+      it 'equals 1' do
+        sum = Fraction.new(1, 0) + Fraction.new(1, 0)
 
-    it 'raise an InvalidFractionError' do
-      expect{ fraction_instance }.to raise_error(InvalidFractionError)
-    end
-  end
-
-  describe '#add' do
-    {
-        '1/2' => '1/6',
-        '1/4' => '1/2'
-    }.each do |adder, result|
-      subject { fraction_instance.add(adder) }
-
-      it { is_expected.to eq result }
+        expect(sum).to eq 1
+      end
     end
   end
 end
