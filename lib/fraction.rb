febb9b9407046c55658f04c0537a11969c0e7659
Jakub Adler <jadler@bookingbug.com>
2017-06-16 18:04:25 +0100

    0 + 1 = 1

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -2,11 +2,14 @@ require 'fraction/version'
 require 'byebug'
 
 class Fraction
+
+  attr_reader :numerator
+
   def initialize(numerator, denominator)
     @numerator = numerator
   end
 
   def +(other)
-    @numerator
+    @numerator + other.numerator
   end
 end
--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -2,35 +2,28 @@ require 'spec_helper'
 
 describe Fraction do
   describe 'Adding fractions' do
-    let(:n_1) {}
-    let(:d_1) {}
-
-    let(:n_2) {}
-    let(:d_2) {}
-
-    let(:fraction_one) { Fraction.new(n_1, d_1) }
-    let(:fraction_two) { Fraction.new(n_2, d_2) }
-
-    subject { fraction_one + fraction_two }
-
     describe '0 + 0' do
-      let(:n_1) { 0 }
-      let(:d_1) { 1 }
+      it 'equals 0' do
+        sum = Fraction.new(0, 1) + Fraction.new(0, 1)
 
-      let(:n_2) { 0 }
-      let(:d_2) { 1 }
-
-      it { is_expected.to eq 0 }
+        expect(sum).to eq 0
+      end
     end
 
     describe '1 + 0' do
-      let(:n_1) { 1 }
-      let(:d_1) { 0 }
+      it 'equals 1' do
+        sum = Fraction.new(1, 1) + Fraction.new(0, 1)
 
-      let(:n_2) { 1 }
-      let(:d_2) { 0 }
+        expect(sum).to eq 1
+      end
+    end
 
-      it { is_expected.to eq 1 }
+    describe '0 + 1' do
+      it 'equals 1' do
+        sum = Fraction.new(0, 1) + Fraction.new(1, 1)
+
+        expect(sum).to eq 1
+      end
     end
   end
 end
