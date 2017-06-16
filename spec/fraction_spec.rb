fc423ec95411913c433a89038db7a11a69affa48
Jakub Adler <jadler@bookingbug.com>
2017-06-16 17:34:22 +0100

    specs refactored

--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -2,20 +2,35 @@ require 'spec_helper'
 
 describe Fraction do
   describe 'Adding fractions' do
-    describe '0 + 0' do
-      it 'equals 0' do
-        sum = Fraction.new(0, 1) + Fraction.new(0, 1)
+    let(:n_1) {}
+    let(:d_1) {}
 
-        expect(sum).to eq 0
-      end
+    let(:n_2) {}
+    let(:d_2) {}
+
+    let(:fraction_one) { Fraction.new(n_1, d_1) }
+    let(:fraction_two) { Fraction.new(n_2, d_2) }
+
+    subject { fraction_one + fraction_two }
+
+    describe '0 + 0' do
+      let(:n_1) { 0 }
+      let(:d_1) { 1 }
+
+      let(:n_2) { 0 }
+      let(:d_2) { 1 }
+
+      it { is_expected.to eq 0 }
     end
 
     describe '1 + 0' do
-      it 'equals 1' do
-        sum = Fraction.new(1, 0) + Fraction.new(1, 0)
+      let(:n_1) { 1 }
+      let(:d_1) { 0 }
 
-        expect(sum).to eq 1
-      end
+      let(:n_2) { 1 }
+      let(:d_2) { 0 }
+
+      it { is_expected.to eq 1 }
     end
   end
 end
